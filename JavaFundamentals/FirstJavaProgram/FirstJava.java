1476c933edc4e21034db7f7e63548799b8c76873
Adeeb Bayat <adeebbayat@yahoo.com>
2023-10-22 17:25:53 -0700

    Initial Commit

--- JavaFundamentals/FirstJavaProgram/FirstJava.java
+++ JavaFundamentals/FirstJavaProgram/FirstJava.java
@@ -2,6 +2,6 @@ public class FirstJava {
     public static void main(String[] args){
         System.out.println("My name is Coding Dojo");
         System.out.println("I am 100 years old");
-        System.out.println("My hometown is Burbank, CA");
+        System.out.println("M hometown is Burbank, CA");
     }
 }
